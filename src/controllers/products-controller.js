4ac77599e0b0686744874f031f8b970d0c315a06
Lucas Oliveira <lucaoxt@gmail.com>
2024-01-22 21:18:52 -0300

    validation

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 class ProductsController {
   static index(req, res, next) {
@@ -20,6 +21,16 @@ class ProductsController {
   }
 
   static create(req, res, next) {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O titulo deve conter pelo menos 3 caracteres.');
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres.');
+    contract.hasMinLen(req.body.description, 3, 'A descrição deve conter pelo menos 3 caracteres.');
+
+    if (!contract.isValid()) {
+      res.status(400).send(contract.errors()).end();
+      return;
+    }
+
     const product = new Product(req.body);
     product
       .save()
